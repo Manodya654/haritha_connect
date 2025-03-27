8111ff84c7469c4d3014ecb2aa9e78286803d998
Hansi Kodamulla <135611607+kodamulla@users.noreply.github.com>
2025-03-27 17:04:06 +0530

    Revert "Add files via upload"
    This reverts commit 6e4a91ffdf898464f4622f28e118ad81b01aa3ec.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp() as Widget);
-
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
     expect(find.text('1'), findsNothing);
@@ -28,6 +25,4 @@ void main() {
   });
 }
 
-class MyApp {
-  const MyApp();
-}
+
