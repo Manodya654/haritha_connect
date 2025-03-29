36a8e5f180154fd2f24294819a0d3692ccb7215d
Hansi Kodamulla <135611607+kodamulla@users.noreply.github.com>
2025-03-29 23:41:42 +0530

    All Completed

---
+++ lib/jobsearch.dart
@@ -0,0 +1,236 @@
+import 'package:flutter/material.dart';
+
+class HarithaConnectApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LandingPage(),
+    );
+  }
+}
+
+class LandingPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('assets/harithaimage1.jpeg', height: 250),
+            SizedBox(height: 20),
+            Text(
+              'Find your job now',
+              style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 20),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 30),
+              child: Text(
+                'Discover the best job opportunities tailored to your skills and interests.',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 16, color: Colors.grey[600]),
+              ),
+            ),
+            
+            SizedBox(height: 30),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                
+                Icon(Icons.circle, size: 10, color: Colors.grey),
+                SizedBox(width: 5),
+                Icon(Icons.circle, size: 10, color: Colors.grey),
+                SizedBox(width: 5),
+                Icon(Icons.circle, size: 10, color: const Color.fromARGB(255, 7, 82, 144)),
+                ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                shape: CircleBorder(),
+                padding: EdgeInsets.all(15),
+                backgroundColor: const Color.fromARGB(255, 43, 103, 167),
+              ),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => JobSearchPage()),
+                );
+              },
+              child: Icon(Icons.arrow_forward, color: Colors.white),
+            ),
+              ],
+              
+            ),
+            SizedBox(width: 100),
+            // ElevatedButton(
+            //   style: ElevatedButton.styleFrom(
+            //     shape: CircleBorder(),
+            //     padding: EdgeInsets.all(15),
+            //     backgroundColor: const Color.fromARGB(255, 43, 103, 167),
+            //   ),
+            //   onPressed: () {
+            //     Navigator.push(
+            //       context,
+            //       MaterialPageRoute(builder: (context) => JobSearchPage()),
+            //     );
+            //   },
+            //   child: Icon(Icons.arrow_forward, color: Colors.white),
+            // ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class JobSearchPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: TextField(
+          decoration: InputDecoration(
+            hintText: 'Search job here...',
+            prefixIcon: Icon(Icons.search, color: Colors.grey),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(30),
+              borderSide: BorderSide.none,
+            ),
+            fillColor: Colors.grey[200],
+            filled: true,
+          ),
+        ),
+        actions: [
+          CircleAvatar(backgroundImage: AssetImage('assets/image.jpeg')),
+          SizedBox(width: 15),
+        ],
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.all(20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.green,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                    child: Text(
+                      'Jobs',
+                      style: TextStyle(color: Colors.white, fontSize: 16),
+                    ),
+                  ),
+                ),
+                SizedBox(width: 100),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color.fromARGB(255, 28, 104, 166),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                    child: Text(
+                      'Events',
+                      style: TextStyle(color: Colors.white, fontSize: 16),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: 5,
+              itemBuilder: (context, index) {
+                List<Color> colors = [
+                  Colors.blue,
+                  Colors.pink,
+                  Colors.green,
+                  Colors.red,
+                  Colors.yellow,
+                ];
+                return Card(
+                  margin: EdgeInsets.symmetric(horizontal: 15, vertical: 8),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.all(15),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: [
+                            CircleAvatar(backgroundColor: colors[index], radius: 20),
+                            SizedBox(width: 12),
+                            Column(
+                              children: [
+                                Row(
+                                  children: [
+                                    Text(
+                                      index == 0 ? 'Web Developer' : 'Software Engineer Intern',
+                                      style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+                                    ),
+                                    SizedBox(width: 100),
+                                    Text(
+                                      '250k - 315k USD/year',
+                                      style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold, color: Colors.grey[700]),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 10),
+                        Wrap(
+                          spacing: 6,
+                          children: [
+                            Chip(label: Text('Full-Time'), backgroundColor: Colors.white),
+                            Chip(label: Text('Hybrid'), backgroundColor: Colors.grey[100]),
+                            Chip(label: Text('Colombo'), backgroundColor: Colors.white),
+                          ],
+                        ),
+                        SizedBox(height: 10),
+                        Text(
+                          'At Vilampara Media, we are leading digital marketing through our innovative Power Reach AI ecosystem, designed to help businesses expand their reach and grow efficiently. As part of this initiative, we are seeking a creative and technically skilled Web Designer to join our team. This role is essential in building, managing, and optimizing WordPress websites that form the foundation of our clients’ digital success.',
+                          style: TextStyle(fontSize: 12, color: Colors.grey[700]),
+                          maxLines: 3,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: Colors.blue,
+        unselectedItemColor: Colors.grey,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.work), label: 'Jobs'),
+          BottomNavigationBarItem(icon: Icon(Icons.book), label: 'Courses'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Account'),
+        ],
+      ),
+    );
+  }
+}
