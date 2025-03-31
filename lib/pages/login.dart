411984b888d6ae7391872ebb2b98058c72045ff0
senuri perera <pereralsd@students.nsbm.ac.lk>
2025-03-31 19:09:34 +0530

    changes

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:ui_connect/pages/signup.dart';
+import 'package:haritha_connect/pages/signup.dart';
 
 class Login extends StatefulWidget {
   const Login({super.key});
