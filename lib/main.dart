970c4d051e42cbb27103bef1907339488dbb90bd
senuri perera <pereralsd@students.nsbm.ac.lk>
2025-04-03 15:21:07 +0530

    deleted routes
    deleted routes in main which are not necessary

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:haritha_connect/pages/courses.dart';
-import 'package:haritha_connect/pages/events.dart';
 import 'package:haritha_connect/pages/jobs.dart';
-import 'package:haritha_connect/pages/profile_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
@@ -20,9 +17,6 @@ void main() async {
     initialRoute: '/home',
     routes: {
       '/home': (context) => Jobs(),
-      '/events': (context) => Events(),
-      '/courses': (context) => Courses(),
-      '/account': (context) => ProfilePage(),
     },
   ));
 }
